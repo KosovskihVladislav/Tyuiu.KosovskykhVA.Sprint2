e73a7d5c467c60e3c9a6131e2eaa3dfb0c2a79a6
Tyuiu.kosovskihVA@outlook.com <mr.kosovskih35@mail.ru>
2023-10-14 16:05:19 +0500

    Task4

---
+++ Tyuiu.KosovskykhVA.Sprint2.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KosovskykhVA.Sprint2.Task4.V21.Lib;
+
+namespace Tyuiu.KosovskykhVA.Sprint2.Task4.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService service1 = new DataService();
+            Assert.AreEqual(0.923, service1.Calculate(3, 3));
+        }
+    }
+}
---
+++ Tyuiu.KosovskykhVA.Sprint2.Task4.V21/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KosovskykhVA.Sprint2.Task4.V21.Lib;
+
+namespace Tyuiu.KosovskykhVA.Sprint2.Task4.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Косовских В. А. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("*Тема: Создания итогового решения по спринту                              *");
+            Console.WriteLine("*Задание #Task4                                                           *");
+            Console.WriteLine("*Вариант #21                                                              *");
+            Console.WriteLine("*Выполнил: Косовских Владислав Алексеевич | ПКТб 23-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение с помощью                ");
+            Console.WriteLine("*     тенарного оператора                                                 *");
+            Console.WriteLine("*                                                                         *");
+
+            // Место решения задания
+            DataService service1 = new DataService();
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.Write("Введите x: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Введите y: ");
+            double y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Результат выражения: " + service1.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
